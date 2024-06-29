209d09ec3365193aac2935e399966f66f0b383c0
kobia77 <kobialen77@gmail.com>
2024-06-29 20:43:17 +0300

    hw3 progress

--- HW3/Client.py
+++ HW3/Client.py
@@ -1,15 +1,39 @@
 
 import socket
+import threading
+import struct
+
+
+ports = [1010, 1020, 1030, 1040, 1050]
+chosenPort = ports[int(input("Pick a port number(0-4): "))]
+clientName= input("What's your name? ")
+
 
 sock = socket.socket (socket.AF_INET, socket. SOCK_STREAM, 0) 
-sock.connect(('127.0.0.1', 9999)) 
-
-
+sock.connect(('127.0.0.1', chosenPort)) 
 print('successful connection')
+
+#registering the client
+header=struct.pack('bbhh',2,1,len(clientName),0)
+sock.send(header)
+sock.send(clientName.encode())
 
 
 while True:
-    data = input('Enter line:').strip().encode ()
-    sock.send(data)
-    reply_data = sock.recv (1024)
-    print('server reply:', reply_data.decode())
+    
+    message = input()
+    header=struct.pack('>bbhh',3,0,len(clientName)+len(message),len(clientName))#ask about the sublen here!!!
+    sock.send(header)
+    sock.send(message.encode())
+
+
+
+    incomingHeader = sock.recv(6)
+    type,subtype,length,sublen = struct.unpack('>bbhh',incomingHeader)
+    print("i got msg")
+    sender,rest = sock.recv(length).decode().split('\0')
+    receiver,text=rest.split(' ',1)
+    print(sender+": "+text)
+
+
+  
--- HW3/Server.py
+++ HW3/Server.py
@@ -3,7 +3,7 @@ import threading
 import struct
 
 myServerConnections = {}
-clientsDict = {}
+clients = {}
 ports = [1010, 1020, 1030, 1040, 1050]
 chosenPort = ports[int(input("Pick a port number(0-4): "))]
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
@@ -12,23 +12,66 @@ sock.bind(('0.0.0.0', chosenPort))
 sock.listen(1)
 
 
+def getKeysByValue(d, target_value):
+    keys = [key for key, value in d.items() if value == target_value]
+    return ' '.join(keys)
+
 def respond_to_client(conn_socket, client_address):
-    data = conn_socket.recv(6)
-    type,subtype,length,sublen = struct.unpack('>bbhh',data)
+    while True:
+        print("waiting for recv\n")
+        data = conn_socket.recv(6)
+        type,subtype,length,sublen = struct.unpack('>bbhh',data)
+        print("recv type:",type," subtype: ",subtype)
 
 
-    if type==0:
-        if subtype==0:
-            newPort=int(conn_socket.recv(4).decode())
-            myServerConnections[newPort]=conn_socket
-            print(list(myServerConnections.keys()))
-            
-            myConnectionsStr=""
-            for server in myServerConnections:
-                myConnectionsStr+= '127.0.0.1'+':'+str(server)+"\0"
-            outGoingData1 = struct.pack('>bbhh',1,0,len(myConnectionsStr)-1,0)
-            conn_socket.send(outGoingData1)
-            conn_socket.send(myConnectionsStr.encode())
+        if type==0:
+            if subtype==0:
+                newPort=int(conn_socket.recv(4).decode())
+                myServerConnections[newPort]=conn_socket
+                print(list(myServerConnections.keys()))
+                myConnectionsStr=""
+                for server in myServerConnections:
+                    myConnectionsStr+= '127.0.0.1'+':'+str(server)+"\0"
+                outGoingData1 = struct.pack('>bbhh',1,0,len(myConnectionsStr)-1,0)
+                conn_socket.send(outGoingData1)
+                conn_socket.send(myConnectionsStr.encode())
+            if subtype==1:
+                pass
+
+        if type==2:
+            if subtype==1:
+                print("handling new client")
+                newClientName=conn_socket.recv(length).decode()
+                clients[newClientName]=conn_socket
+                print(list(clients.keys()))
+
+        if type==3:
+            print("forwarding msg to a client")
+            x=conn_socket.recv(length).decode()
+            print(x)
+            receiver,text=x.split(' ',1)
+            sender=getKeysByValue(clients,conn_socket)
+            msg = sender+'\0'+receiver+' '+text   
+
+
+            header=struct.pack('>bbhh',3,0,len(msg),len(sender))
+            print("packed the msg")
+            clients[receiver].send(header)
+            clients[receiver].send(msg.encode())
+            print("sent the msg")
+
+            #kobi hey
+            # sender adi 
+
+
+
+
+
+        if type==8:
+            incomingData = conn_socket.recv(4)
+            portForUpdate=int(incomingData.decode())
+            myServerConnections[portForUpdate] = conn_socket
+            print("my dict is: ",list(myServerConnections.keys()))
 
 
 # 1000:(127.0.0.1,4321)
@@ -45,13 +88,17 @@ def splitNewString(longString):
 def connectToOtherServers(portsList):
     nportsList = [int(num) for num in portsList]
     for port in nportsList:
-        print(port," ",chosenPort)
+        #print(port," ",chosenPort)
         if port!=chosenPort:
             tempsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
             tempsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             tempsock.connect(('127.0.0.1', int(port)))
             print("adding port ",port)
             myServerConnections[port]=tempsock
+            header = struct.pack('>bbhh',8,0,0,0) # sending my port to who i connected to, for him to add me also to his dict
+            tempsock.send(header)
+            outGoingData = str(chosenPort).encode()
+            tempsock.send(outGoingData)
 
 
 def actAsClient():
@@ -66,12 +113,12 @@ def actAsClient():
                 infoAsk = struct.pack('>bbhh',0,0,0,0)
                 tempsock.send(infoAsk)
                 tempsock.send(str(chosenPort).encode())
+                print("sent info and chosenport\n")
                 firstRecieved = tempsock.recv(6)
                 type,subtype,length,sublen=struct.unpack('>bbhh',firstRecieved)
                 secondRecieved = tempsock.recv(length)
-                print("second: ",secondRecieved.decode())
                 secondRecieved=secondRecieved.decode()
-                portsToAdd=splitNewString(secondRecieved)
+                portsToAdd=splitNewString(secondRecieved)#handling the other servers list
                 connectToOtherServers(portsToAdd)
                 print(list(myServerConnections.keys()))
 
